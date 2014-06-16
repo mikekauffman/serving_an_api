4588ecb71eadfd1ca8dd73773ac92d9c4e54b482
Mike Kauffman <mike.a.kauffman@gmail.com>
2014-06-16 13:14:56 -0600

    [#73321046] can make a request to /cars/:id
    successful response includes car details, if no car is found a 404 response is returned.
    
    Signed-off-by: Nathanael Burt <nathanael.burt@gmail.com>

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,4 +2,12 @@ class CarsController < ActionController::Base
   def index
     @cars = Car.all
   end
+
+  def show
+    begin
+      @car = Car.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render :status => 404, :json => {}
+    end
+  end
 end
--- spec/api/car_spec.rb
+++ spec/api/car_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
 describe 'Car API' do
+
+  before do
+    @ford = create_make(name: "Ford")
+    @bronco = create_car(color: "red", doors: 4, purchased_on: "1973-10-04", make_id: @ford.id)
+  end
+
   describe 'Get /cars response' do
     it 'returns a list of cars' do
-      ford = create_make(name: "Ford")
-      bronco = create_car(color: "red", doors: 4, purchased_on: "1973-10-04", make_id: ford.id)
-      mustang = create_car(color: "blue", doors: 2, purchased_on: "2012-01-24", make_id: ford.id)
-
+      mustang = create_car(color: "blue", doors: 2, purchased_on: "2012-01-24", make_id: @ford.id)
 
       get '/cars', {}, {'Accept' => 'application/json'}
 
@@ -21,13 +24,13 @@ describe 'Car API' do
             {
               "_links" => {
                 "self" => {
-                  "href" => "/cars/#{bronco.id}"
+                  "href" => "/cars/#{@bronco.id}"
                 },
                 "make" => {
-                  "href" => "/makes/#{ford.id}"
+                  "href" => "/makes/#{@ford.id}"
                 }
               },
-              "id" => bronco.id,
+              "id" => @bronco.id,
               "color" => "red",
               "doors" => 4,
               "purchased_on" => "1973-10-04"
@@ -38,7 +41,7 @@ describe 'Car API' do
                   "href" => "/cars/#{mustang.id}"
                 },
                 "make" => {
-                  "href" => "/makes/#{ford.id}"
+                  "href" => "/makes/#{@ford.id}"
                 }
               },
               "id" => mustang.id,
@@ -51,6 +54,36 @@ describe 'Car API' do
       }
       expect(response.code.to_i).to eq 200
       expect(JSON.parse(response.body)).to eq expected
+    end
+  end
+
+  describe 'get /cars/:id' do
+    it 'returns details about a car' do
+      get "/cars/#{@bronco.id}", {}, {'Accept' => 'application/json'}
+
+      expected = {
+        "_links" => {
+          "self" => {
+            "href" => "/cars/#{@bronco.id}"
+          },
+          "make" => {
+            "href" => "/makes/#{@ford.id}"
+          }
+        },
+        "id" => @bronco.id,
+        "color" => "red",
+        "doors" => 4,
+        "purchased_on" => "1973-10-04"
+      }
+      expect(response.code.to_i).to eq 200
+      expect(JSON.parse(response.body)).to eq expected
+    end
+
+    it 'will render a 404 if the car does not exist' do
+      get "/cars/1", {}, {'Accept' => 'application/json'}
+
+      expect(response.code.to_i).to eq 404
+      expect(JSON.parse(response.body)).to eq({})
     end
   end
 end
